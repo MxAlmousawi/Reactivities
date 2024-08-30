79cd632ba527989e474e52eab4ff9d5954a38b76
mohammad <mohammadalmoussawiabc@gmail.com>
2024-08-30 19:42:45 +0300

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<Activity>> GetActivities([FromRoute]Guid id)
+        {
+            return await context.Activities.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
