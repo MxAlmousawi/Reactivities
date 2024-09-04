990f6d5c25a2d70cdfea19dacc2d2da61319e5ce
mohammad <mohammadalmoussawiabc@gmail.com>
2024-09-04 18:58:15 +0300

    end of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, Header, List, ListItem } from "semantic-ui-react";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/Activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((act: any) => {
+          return <ListItem key={act.id}>{act.title}</ListItem>;
+        })}
+      </List>
+    </div>
+  );
+}
+
+export default App;
