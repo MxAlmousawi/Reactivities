673e70abe4084bea2ea05cc86d0dde3e93ee4e99
mohammad <mohammadalmoussawiabc@gmail.com>
2024-12-30 20:30:28 +0200

    end of section 12

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(UserManager<AppUser> userManager, TokenService tokenService) : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager = userManager;
+        private readonly TokenService tokenService = tokenService;
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (result)
+            {
+                return Ok(CreateUserObject(user));
+            }
+
+            return Unauthorized();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if (await userManager.FindByEmailAsync(registerDto.Email) != null) return BadRequest("Email taken");
+
+            if (await userManager.FindByNameAsync(registerDto.Username) != null) return BadRequest("Username taken");
+
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Username
+            };
+
+            var result = await userManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(CreateUserObject(user));
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            var user = await userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return Ok(CreateUserObject(user));
+        }
+
+        private UserDto CreateUserObject(AppUser user)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Token = tokenService.CreateToken(user),
+                Username = user.UserName,
+                Image = null
+            };
+        }
+    }
+}
